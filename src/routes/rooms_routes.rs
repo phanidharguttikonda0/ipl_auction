3ad6cc0eae3d5dd5aa018d9725b1f63a8cfab51d
Phanidhar reddy guttikonda <phanidharguttikonda0@gmail.com>
2025-11-14 15:44:12 +0530

    Update rooms_routes.rs

--- src/routes/rooms_routes.rs
+++ src/routes/rooms_routes.rs
@@ -11,7 +11,7 @@ pub fn rooms_routes() -> Router<Arc<AppState>> {
         .route("/join-room-get-teams/{room-id}", get(get_remaining_teams))// it returns the remaining teams
         .route("/join-room/{room-id}/{team_name}", get(join_room)) // it returns the participant_id
         .route("/get-auctions-played", get(get_rooms_played)) // it going to return the list of room-ids participated by the user and along with date
-        .route("/get-participants", get(get_participants_room)) // it going to return the list of participants-id along with the team-name, using these participant_ids to get the team details and player details
+        .route("/get-participants/{room_id}", get(get_participants_room)) // it going to return the list of participants-id along with the team-name, using these participant_ids to get the team details and player details
         .layer(middleware::from_fn(crate::middlewares::authentication::auth_check))
 
 }
